1b9d3ca57fc03e6acf8898aa089767bfdd0287d1
Balaji KS <balajiks@cgvakindia.com>
2019-11-07 21:10:12 +0530

    Initial Commit

---
+++ FetchUsersCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FetchUsersCoreData
+//
+//  Created by Bala KS on 07/11/19.
+//  Copyright © 2019 Bala KS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
